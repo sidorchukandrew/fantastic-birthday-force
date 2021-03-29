d78748465c707871d5a94f905c090e43a27db3f7
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2021-03-29 17:20:10 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Application from './Application';
+import 'semantic-ui-css/semantic.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Application />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Application.js
@@ -0,0 +1,11 @@
+import { Button } from "semantic-ui-react";
+
+export default function Application() {
+    return (
+        <div>
+            <div className="d-flex justify-end">
+                <Button color="blue">Submit Your Application</Button>
+            </div>
+        </div>
+    );
+}
